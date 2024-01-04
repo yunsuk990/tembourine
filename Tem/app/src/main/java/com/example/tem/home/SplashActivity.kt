d54dc5e73cff75f3e5b3cd056a24b07a325c9e66
yunsuk990 <yunsuk990@naver.com>
2024-01-04 14:35:31 +0900

    Update SplashActivity

---
+++ Tem/app/src/main/java/com/example/tem/home/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tem.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tem.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
