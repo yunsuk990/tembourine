4cd9a7e9518fdb6421d8317945493f0a6532d30b
yunsuk990 <yunsuk990@naver.com>
2024-01-04 14:30:35 +0900

    Update

--- Tem/app/src/main/java/com/example/tem/home/HomeFragment.kt
+++ Tem/app/src/main/java/com/example/tem/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tem.R
+
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
