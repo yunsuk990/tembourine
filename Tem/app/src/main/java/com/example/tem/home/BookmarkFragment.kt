ce069bb0c585a20eb6e4526f257ace4cd7e47a49
yeoroJeong <packageair@naver.com>
2024-02-06 22:59:48 +0900

    feat: Viewpager2 제거

--- Tem/app/src/main/java/com/example/tem/home/BookmarkFragment.kt
+++ Tem/app/src/main/java/com/example/tem/home/BookmarkFragment.kt
@@ -1,19 +1,25 @@
 package com.example.tem.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.tem.BookmarkFir
 import com.example.tem.BookmarkSec
+import com.example.tem.ItemAdapter
+import com.example.tem.ItemData
 import com.example.tem.R
 import com.example.tem.databinding.FragmentBookmarkBinding
 
 class BookmarkFragment : Fragment() {
     private lateinit var binding: FragmentBookmarkBinding
+    lateinit var itemAdapter: ItemAdapter
+    val datas = mutableListOf<ItemData>()
     private fun changeFragment(fragment: Fragment, title: String) {
         val fragmentManager = requireActivity().supportFragmentManager
         fragmentManager.beginTransaction()
@@ -24,8 +30,7 @@ class BookmarkFragment : Fragment() {
 
         // 화면에 표시할 Fragment 리스트
         val pageList = listOf(
-            BookmarkFir(),
-            BookmarkSec()
+            BookmarkFir()
         )
 
         override fun getItemCount(): Int {
@@ -51,17 +56,24 @@ class BookmarkFragment : Fragment() {
         binding.backBtn.setOnClickListener {
             changeFragment(HomeFragment(),"1")
         }
-        binding.bmarkVp.apply {
-            adapter = pagerAdpater
-            // 사용자의 Swipe 동작에 의해 화면이 이동하지 않도록 설정
-            isUserInputEnabled = true
-            /*
-             * Viewpager2의 메모리 상에 Load시켜주는 화면 숫자 설정
-             * 해당 예시는 화면의 숫자만큼 Load하도록 설정했다.
-             * 메모리 관리에 유의하여 사용해야 한다.
-            */
-            offscreenPageLimit = pagerAdpater.itemCount
-        }
+        initRecycler()
+        binding.itemRv.layoutManager = LinearLayoutManager(requireContext())
         return view
+    }
+    private fun initRecycler() {
+        itemAdapter = ItemAdapter(requireContext())
+        binding.itemRv.adapter = itemAdapter
+
+
+        datas.apply {
+            add(ItemData(img = R.drawable.coupang, name = "수건"))
+            add(ItemData(img = R.drawable.coupang, name = "화장지"))
+
+            itemAdapter.datas = datas
+            itemAdapter.notifyDataSetChanged()
+            Log.d("chk",datas.size.toString())
+            Log.d("ch",datas.first().name)
+
+        }
     }
 }
